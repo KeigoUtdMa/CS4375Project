d2e7a54aba0e5d7482aa3730b88ec883b6f4d61a
KeigoUtdMa <54554309+KeigoUtdMa@users.noreply.github.com>
2020-11-22 11:51:43 -0600

    New321

--- Project.py
+++ Project.py
@@ -12,9 +12,9 @@ from keras.layers import Dense
 import tensorflow as tf
 import tensorflow_hub as hub
 
-g = Github("KeigoUtdMa", "abc8899123")
+g = Github("6c56c746c76f6528c2afbf70b308ab8868404a8a")
 repo = g.get_user().get_repo("CS4375Project")
-"""
+
 epochs = 100
 numtrainImages = 1000
 numtestImages = 400
@@ -22,7 +22,7 @@ trainimagedata = np.zeros((numtrainImages, 128, 128, 3))
 testimagedata = np.zeros((numtestImages, 128, 128, 3))
 shape = (128, 128)
 
-trains = repo.get_contents("train")
+trains = repo.get_contents("training_set")
 trainfilenames = []
 traincategories = []
 for file in trains:
@@ -37,7 +37,7 @@ for file in trains:
 traincategories = to_categorical(traincategories)
 traincount = 0
 for name in trainfilenames:
-  trainresponse = requests.get("https://github.com/WretchIV/CS4372A3/blob/master/train/" + name + "?raw=true")
+  trainresponse = requests.get("https://github.com/KeigoUtdMa/CS4375Project/blob/main/training_set/" + name + "?raw=true")
   trainimage = np.array(Image.open(BytesIO(trainresponse.content)).resize(shape))
   trainimagedata[traincount] = trainimage
   traincount += 1
@@ -57,9 +57,10 @@ datagen = ImageDataGenerator(
     width_shift_range = 0.1,
     height_shift_range = 0.1)
 
-tests = repo.get_contents("test")
+tests = repo.get_contents("test_set")
 testfilenames = []
 testcategories = []
+
 for file in tests:
   testfilename = file.name
   testcategory = testfilename.split('.')[0]
@@ -70,8 +71,9 @@ for file in tests:
     testcategories.append(1)
 testcategories = to_categorical(testcategories)
 testcount = 0
+
 for name in testfilenames:
-  testresponse = requests.get("https://github.com/WretchIV/CS4372A3/blob/master/test/" + name + "?raw=true")
+  testresponse = requests.get("https://github.com/KeigoUtdMa/CS4375Project/blob/main/test_set/" + name + "?raw=true")
   testimage = np.array(Image.open(BytesIO(testresponse.content)).resize(shape))
   testimagedata[testcount] = testimage
   testcount += 1
@@ -96,4 +98,3 @@ plt.xlabel("Epochs")
 plt.ylabel("Loss")
 plt.legend(["training", "testing"], loc = "upper right")
 plt.show()
-"""
